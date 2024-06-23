bfe303c9ee8959354b16ba703f32cca3d9867d63
123vkgithub <kumbhalkarvaibhavi@gmail.com>
2024-06-23 13:21:53 +0530

    Footer added

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,22 +1,43 @@
+import React from 'react';
 import "./footer.css"
 
-const Footer = ({ address, phone, socialLinks }) => {
-  return (
-    <footer className="footer">
-      <div className="footer-content">
-        <p>{address}</p>
-        <p>Phone: {phone}</p>
-        <div className="social-icons">
-          {socialLinks.map((link) => (
-            <a key={link.id} href={link.url} target="_blank" rel="noopener noreferrer">
-              <img src={link.icon} alt={link.name} />
-            </a>
-          ))}
-        </div>
-        <p>© {new Date().getFullYear()} Cake Shop</p>
-      </div>
-    </footer>
-  );
+const Footer = ({ links }) => {
+    return (
+        <footer>
+            <ul>
+                <li className='head'>OUR ACCOUNTS</li>
+                {links.map((link, index) => (
+                    <li key={index}>
+                        <a href={link.url}>
+                            {link.text}
+                            {link.icon && <i className={link.icon}></i>}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+
+            <div className="services">
+                <p className='head'>OUR DELICIOUS CAKE SERVICES</p>
+                    <ul>
+                        <li className='f-text'>Custom cakes</li>
+                        <li className='f-text'>Cupcakes</li>
+                        <li className='f-text'>Wedding cakes</li>
+                    </ul>
+            </div>
+
+            <div className="address-contact">
+                <p className='head'>VISIT US </p>
+                    <address className='f-text'>
+                        Near Durga Mandir,Tumsar<br /> Cakeville, Deliciousland
+                    </address>
+                <p className='head'>CONTACT US</p>
+                    <ul>
+                        <li className='f-text'>Phone: +1 (123) 456-7890</li>
+                        <li className='f-text'>Email: info@shree-cakes.com</li>
+                    </ul>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -1,32 +1,36 @@
-import "./home.css"
-import {features, siteDetails, theme} from "./../../data"
-import FeatureCard from "./../../components/Feature Card/featurecard"
-import Footer from "./../../components/Footer/footer"
+import "./home.css";
+import { features, siteDetails, theme } from "../../data";
+import FeatureCard from "../../components/Feature Card/featurecard";
+import Footer from "../../components/Footer/footer";
 
-function home() {
+const Home = () => {
+  const footerLinks = [
+    // { text: "Home", url: "/" },
+    { icon: 'fab fa-facebook', text: "Facebook ", url: "https://www.facebook.com/p/Shree-cakes-100071162995912/"},
+    { text: "Twitter ", url: "https://x.com/ghanshyam_baker?lang=en&mx=2", icon: 'fab fa-twitter' },
+    { text: "Instagram ", url:"https://www.instagram.com/sribakery_trichy/", icon: 'fab fa-instagram'}
+  ];
+
   return (
     <div>
-        <h1  className="title" style ={{color: theme.dark}}>
-            {siteDetails.title}
-        </h1>
-        <p className="description">{siteDetails.description}</p>
+      <h1 className="title" style={{ color: theme.dark }}>
+        {siteDetails.title}
+      </h1>
+      <p className="description">{siteDetails.description}</p>
 
-    {siteDetails.showFeatures ? 
+      {siteDetails.showFeatures ? (
         <div className="features">
-            {
-                features.map((feature, i)=>{
-                    const{title, img}  = feature
+          {features.map((feature, i) => {
+            const { title, img } = feature;
 
-                    return(
-                       <FeatureCard key={i} title={title} img={img} />
-                    )
-                })
-            }
-        </div> 
-        :
-         null}
+            return <FeatureCard key={i} title={title} img={img} />;
+          })}
+        </div>
+      ) : null}
+
+      <Footer links= {footerLinks} />
     </div>
-  )
-}
+  );
+};
 
-export default home
+export default Home;
